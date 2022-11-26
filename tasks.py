04c4d997250505460fb7a71f0b9f2c9f50651dce
FanderV <113282846+FanderV@users.noreply.github.com>
2022-11-26 13:14:12 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -63,7 +63,7 @@ print("Простые числа этого диапозона: ")
 for num in range(down, up +1):
     if num > 1:
         for i in range(2, num):
-            if (num % 1) == 0:
+            if (num % i) == 0:
                 break
         else:
             print(num)
